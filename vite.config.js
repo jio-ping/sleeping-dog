5df7f880fe896bfb8c86b9532dbaf7f9b75deb8c
jio <pennicillium@daum.net>
2024-03-30 22:35:08 +0900

    [Chore]scaffolding

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+    }),
+  ],
+});
