8ec9205e3ed4cb21ecf8004773e928b2ae98b83f
jio <pennicillium@daum.net>
2024-04-08 00:53:10 +0900

    [Feat]모달 닫기 함수 연결 #1

--- src/components/Molecules/Modal/Modal.tsx
+++ src/components/Molecules/Modal/Modal.tsx
@@ -1,12 +1,19 @@
-export default function Modal() {
+interface ModalProps {
+  closeFn: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function Modal({ closeFn }: ModalProps) {
   return (
-    <div className="w-[80%] h-[169px] text-white bg-black border rounded-[3px] flex flex-col justify-center items-center ">
+    <div className=" font-normal font-['TTLaundryGothicB'] w-[70%] h-[169px] text-white bg-black border rounded-[3px] flex flex-col justify-center mx-auto ">
       <div className="px-[77px] text-center py-[30px]">
         <h3 className="text-[30px]">정상이에요!</h3>
-        <p>2024.04.06 18:30 꽁이 20회/분</p>
+        <p className="text-dark-gray">2024.04.06 18:30 꽁이 20회/분</p>
       </div>
-      <div className="flex-grow w-full flex  border-t border-white ">
-        <button className="flex-grow border-r border-white py-[7px]">
+      <div className=" font-sans flex-grow w-full flex  border-t border-white ">
+        <button
+          onClick={closeFn}
+          className="flex-grow border-r border-white py-[7px]"
+        >
           닫기 🖐️
         </button>
         <button className="flex-grow py-[7px]">기록 📝</button>
--- src/components/Page/Record/Record.tsx
+++ src/components/Page/Record/Record.tsx
@@ -1,10 +1,16 @@
 import { useState, useEffect } from "react";
 
+import Modal from "@/components/Molecules/Modal/Modal.tsx";
 /*
-1. 시작버튼 터치시 
+
+1. 시작버튼 터치 시 
   버튼 텍스트 변경
   초 단위로 시간 시간 단축 
 2. 터치 버튼 클릭시 카운트 추가
+3. 잔여 초가 0일때 결과를 담은 모달 노출
+  기록 -> localstorage에 저장 / 초기화
+  닫기 -> 초기화
+4. 강아지 input에 이름 작성하면 새로고침할때도 이름 저장
 
 */
 
@@ -12,6 +18,7 @@ export default function Record() {
   const [measurement, setMeasurement] = useState(false);
   const [remainTime, setRemainTime] = useState(60);
   const [count, setCount] = useState(0);
+  const [modal, setModal] = useState(false);
 
   useEffect(() => {
     let countId: string | number | NodeJS.Timeout | undefined;
@@ -22,7 +29,8 @@ export default function Record() {
       );
     } else if (remainTime === 0) {
       setMeasurement(false);
-      setRemainTime(10);
+      setRemainTime(60);
+      setModal(true);
     }
     return () => clearInterval(countId);
   }, [measurement, remainTime]);
@@ -31,7 +39,7 @@ export default function Record() {
     <>
       <input type="text" placeholder="강아지이름" />
       <button>모드변경</button>
-      <div>
+      <div className="font-['TTLaundryGothicB']">
         <p>
           남은 시간 <em>{remainTime}</em> 초
         </p>
@@ -43,6 +51,7 @@ export default function Record() {
         {measurement ? "멈춤" : "시작"}
       </button>
       <button onClick={() => setCount((count) => count + 1)}>+1</button>
+      {modal ? <Modal closeFn={() => setModal(false)} /> : ""}
     </>
   );
 }
