f937aa9f65518c941bfee5bb342919cee9214d2b
jio <pennicillium@daum.net>
2024-04-08 00:44:16 +0900

    [Setting]스타일링을 위한 설정파일, 웹폰트 설정 #1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: { "dark-gray": "D9D9D9", "light-black": "141414" },
+    },
   },
   plugins: [],
 };
