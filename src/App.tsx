d7bcf724f64ef95b25338716fa3496b9bd46aeb7
jio <pennicillium@daum.net>
2024-04-09 14:33:16 +0900

    [Style]배경화면 설정 #3

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import Record from "@/components/Page/Record/Record.tsx";
 
 export default function App() {
   return (
-    <div className="m-auto h-svh max-w-screen-sm">
+    <div
+      className={`m-auto h-svh max-w-screen-sm bg-[url('/public/darkmode.svg')] bg-no-repeat bg-cover bg-bottom `}
+    >
       <Record />
     </div>
   );
