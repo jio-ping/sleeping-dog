b912581d5b9b9013046b36444d17293b6518d5c0
jio <pennicillium@daum.net>
2024-04-09 18:05:36 +0900

    [Feat] 다크모드 적용 #3

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Record from "@/components/Page/Record/Record.tsx";
 export default function App() {
   return (
     <div
-      className={`m-auto h-svh max-w-screen-sm bg-[url('/public/darkmode.svg')] bg-no-repeat bg-cover bg-bottom `}
+      className={`m-auto h-svh max-w-screen-sm bg-[url("/public/lightmode.svg")] bg-cover bg-bottom bg-no-repeat dark:bg-[url('/public/darkmode.svg')]`}
     >
       <Record />
     </div>
--- src/components/Atom/Input/Input.tsx
+++ src/components/Atom/Input/Input.tsx
@@ -1,13 +1,8 @@
 /* 로컬스토리지에 저장된 이름이 있다면 바로 value에 써주고 없으면 반려동물의 이름을 입력해주세요 
 엔터 누르면 자동으로 로컬스토리지에 저장? | 디바운싱 적용해서 로컬스토리지에 저장? -> 상태관리해야됨 | 그냥 초점 이동하면 저장?
 ! */
-// import { useEffect } from "react";
-export default function Input() {
-  const mode = {
-    dark: "dark-txt-1",
-    light: "black",
-  };
 
+export default function Input() {
   const handleSaveName = () => {
     const name = document.querySelector<HTMLInputElement>("#petName");
     if (name) {
@@ -19,7 +14,7 @@ export default function Input() {
     <>
       <input
         id="petName"
-        className={`py-3 text-[20px] font-['TTLaundryGothicB'] bg-transparent w-[80px] outline-none text-${mode["light"]}  border-b-2 border-${mode.light} text-center`}
+        className={`w-[80px] border-b-2 border-black bg-transparent py-3 text-center font-['TTLaundryGothicB'] text-[20px] text-black  outline-none dark:border-dark-txt-1 dark:text-dark-txt-1`}
         type="text"
         placeholder="이름🐶🐱"
         onBlur={handleSaveName}
