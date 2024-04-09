54ac768ce5238df85becc3b5d8be1ed57fe449f7
jio <pennicillium@daum.net>
2024-04-09 21:32:34 +0900

    [Feat] 모달 컴포넌트 디스플레이 모드 적용 #4

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "dark-gray": "D9D9D9",
-        "light-black": "141414",
+        "dark-gray": "#D9D9D9",
+        "light-black": "#141414",
         "dark-txt-1": "rgba(232, 228, 228, 1)",
       },
     },
