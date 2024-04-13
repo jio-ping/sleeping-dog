be3154bc7f237fc1093e93a3536cf987c32b2a02
jio <pennicillium@daum.net>
2024-04-13 22:45:54 +0900

    Revert "[Feat] 버튼 컴포넌트 타입 지정, 함수 연결 #3"
    This reverts commit 5c99ee767ab5735ec3df8a7cbd5f046e4eda3a2d.

--- src/components/Atom/CountButton/CountButton.tsx
+++ src/components/Atom/CountButton/CountButton.tsx
@@ -1,16 +1,14 @@
 import useMode from "@/store/useMode";
 
 type Button = {
-  handleFn: () => void;
-  children: React.ReactNode;
+  children: string;
 };
 
-export default function CountButton({ handleFn, children }: Button) {
+export default function CountButton({ children }: Button) {
   const darkmode = useMode((state) => state.darkmode);
 
   return (
     <button
-      onClick={handleFn}
       className={`rounded-[30px] border-4 px-7 py-2 font-['TTLaundryGothicB'] text-xl ${darkmode ? "  border-dark-gray text-dark-gray" : "border-light-black text-light-black"}`}
     >
       {children}
