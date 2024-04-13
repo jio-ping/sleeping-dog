676fc282cde8ac8c97a15f0f09fcc8a6de200651
jio <pennicillium@daum.net>
2024-04-13 22:47:52 +0900

    Revert "[Modify] 전역 관리 메소드,상태로 변경 #5"
    This reverts commit afd9ddf42f2fb3ad2e9c8ae0316897bcc0edcafc.

--- src/components/Atom/Input/Input.tsx
+++ src/components/Atom/Input/Input.tsx
@@ -1,24 +1,24 @@
+/* 로컬스토리지에 저장된 이름이 있다면 바로 value에 써주고 없으면 반려동물의 이름을 입력해주세요 
+엔터 누르면 자동으로 로컬스토리지에 저장? | 디바운싱 적용해서 로컬스토리지에 저장? -> 상태관리해야됨 | 그냥 초점 이동하면 저장?
+! */
 import useMode from "@/store/useMode";
-import useStorage from "@/store/useStorage";
 export default function Input() {
-  const { setName, name } = useStorage((state) => state);
-  const darkmode = useMode((state) => state.darkmode);
   const handleSaveName = () => {
     const name = document.querySelector<HTMLInputElement>("#petName");
     if (name) {
-      setName(name.value);
+      localStorage.setItem("petName", name.value);
     }
   };
+  const darkmode = useMode((state) => state.darkmode);
   return (
     <>
       <input
         id="petName"
-        autoComplete="off"
         className={`h-12 w-[80px] border-b-2 bg-transparent text-center font-['TTLaundryGothicB'] text-xl  outline-none 
         
         ${darkmode ? "border-dark-txt-1 text-dark-txt-1 " : " border-black text-black"}`}
         type="text"
-        placeholder={name == "" ? "이름🐶🐱" : name}
+        placeholder="이름🐶🐱"
         onBlur={handleSaveName}
       />
     </>
