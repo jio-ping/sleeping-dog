fb965e5d8d9ae5620e2857fe48dd8af19ccdff43
jio <pennicillium@daum.net>
2024-04-13 19:04:42 +0900

    [Feat] 측정시간 유틸함수 #4

---
+++ src/utils/getTime.ts
@@ -0,0 +1,6 @@
+const getTime = () => {
+  const date = new Date();
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
+};
+
+export default getTime;
