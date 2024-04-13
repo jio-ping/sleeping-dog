fa03eb052501ac2e3a7fc3d1849aacc465c12fc2
jio <pennicillium@daum.net>
2024-04-13 22:41:56 +0900

    [Feat] useStorage 기록 상태 관리 함수 구현 #5

--- src/store/useStorage.ts
+++ src/store/useStorage.ts
@@ -1,16 +1,27 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
+type Record = {
+  dateTime?: string;
+  counts?: number;
+};
 interface StorageState {
   name: string;
   setName: (enteredName: string) => void;
+  record: Record[];
+  setRecord: ({ dateTime, counts }: Record) => void;
 }
 
 const useStorage = create<StorageState>()(
   persist(
-    (set) => ({
+    (set, get) => ({
       name: "",
       setName: (enteredName) => set({ name: enteredName }),
+      record: [],
+      setRecord: ({ dateTime, counts }) => {
+        const newRecord = [...get().record, { dateTime, counts }];
+        set({ record: newRecord });
+      },
     }),
     {
       name: "petState",
