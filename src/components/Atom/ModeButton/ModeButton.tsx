fd2d5bc8e6e91de359bfa4174d1c5d090be54335
jio <pennicillium@daum.net>
2024-05-02 23:40:39 +0900

    [Style] modeButton 스타일링 수정 #3

--- src/components/Atom/ModeButton/ModeButton.tsx
+++ src/components/Atom/ModeButton/ModeButton.tsx
@@ -1,20 +1,20 @@
 import darkmodeIcon from "@/assets/darkmode.svg";
 import lightmodeIcon from "@/assets/lightmode.svg";
-import useMode from "@/store/useMode";
 
+import useMode from "@/store/useMode";
 export default function ModeButton() {
   const { darkmode, convertMode } = useMode((state) => state);
   return (
-    <button onClick={convertMode}>
+    <button className="h-16 w-20 " onClick={convertMode}>
       <figure className="flex flex-col items-center justify-around dark:text-dark-txt-1">
         {darkmode ? (
           <img className="w-8" src={darkmodeIcon} />
         ) : (
           <img className="w-8" src={lightmodeIcon} />
         )}
-        <figcaption>
+        <figcaption className="justify-self-end">
           {darkmode ? (
-            <span className="text-xs text-dark-txt-1">라이트모드적용 </span>
+            <span className=" text-xs text-dark-txt-1">라이트모드적용 </span>
           ) : (
             <span className="text-xs text-black">다크모드적용</span>
           )}
