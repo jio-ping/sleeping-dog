e3d7c03f9fa1f26b8842a3234c7391b5e0b8b6a1
jio <pennicillium@daum.net>
2024-05-10 23:28:52 +0900

    [Modify] 오타 수정 #2

--- src/utils/testResult.ts
+++ src/utils/testResult.ts
@@ -1,6 +1,6 @@
 export default function testResult(counts: number): string {
   if (10 <= counts && counts <= 25) {
-    return "정상법위예요 ☺️";
+    return "정상범위예요 ☺️";
   } else {
     return "지속적인 관찰이 필요해요 😵‍💫";
   }
