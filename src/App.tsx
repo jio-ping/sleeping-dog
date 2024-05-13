2cdaf568711c7a31929b2ed85344707208881a92
jio <pennicillium@daum.net>
2024-05-13 19:58:54 +0900

    [Bugfix] 메타태그 미적용 수정 #14

--- src/App.tsx
+++ src/App.tsx
@@ -12,21 +12,13 @@ export default function App() {
       className={`${darkmode ? "bg-[url('/darkmode.svg')]" : 'bg-[url("/lightmode.svg")]'} m-auto h-svh max-w-screen-sm bg-cover bg-bottom bg-no-repeat `}
     >
       <Helmet>
-        <meta name="반려동물 호흡수 측정" />
-        <meta property="og:site" content="반려동물 호흡수 측정" />
-        <meta
-          property="description"
-          content="자고있는 우리 아이의 호흡 수를 측정하고 변화를 기록해요 ! 연속성 있는 기록을 위해 같은 브라우저 환경에서 접속해주세요.
-        "
-        />
         <meta
           property="og:title"
           content={
             currentState === "record" ? "호흡수 측정하기" : "호흡수 기록보기"
           }
         />
-        <meta property="og:image" content={"ogimg.png"} />
-        <meta name="twiter:image" content={"ogimg.png"} />
+    
         <title>반려동물 호흡수 측정</title>
         <link
           rel="icon"
